87247ff1cef63ec4d37418e7dd989b142a3448c5
Deyvison Silva <drzsilva1@gmail.com>
2021-07-15 13:31:59 -0300

    aula 48: pipe async

--- src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-exemplos-pipes',
   templateUrl: './exemplos-pipes.component.html',
@@ -19,6 +22,12 @@ export class ExemplosPipesComponent implements OnInit {
   livros: string[] = ['Angular', 'React'];
 
   filtro: string = '';
+
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Valor assíncrono"), 2000);
+  });
+
+  valorAsync2 = interval(2000).pipe(map(valor => 'Valor assíncrono 2'));
 
   constructor() { }
 
